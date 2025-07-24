6dcdb80e826fc3f3c4ca9151ba78e645efa02829
Roberto Lourenço <robertolourenc0@outlook.com>
2025-07-24 03:10:45 -0300

    chore: rename the project ClasseMetodo to AlunoExercicio

--- ClassesMetodos/Program.cs
+++ ClassesMetodos/Program.cs
@@ -1,3 +1,5 @@
 ﻿var aluno = AlunoFactory.Criar("Testando", "2005/01/01", "Masculino");
+var aluno2 = AlunoFactory.Criar("Teste", "2005/01/01", "Masculino");
 aluno.AtualizarNotas(8, 4, 8);
 Console.WriteLine(aluno);
+Console.WriteLine("\n"+aluno2);
